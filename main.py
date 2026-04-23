4608c8b2a9b0042c4919b9bbbc8bef342a13db71
Janek <jan.p.przystal@gmail.com>
2026-04-23 13:15:45 +0200

    fix

--- main.py
+++ main.py
@@ -99,6 +99,7 @@ def spec_experiment(experiment: experiment.Experiment):
     config.USE_ROOT_PRIORITY = experiment.root
     config.PROFILING_REPETITIONS = experiment.profiling_repetitions
     config.USE_INTERPOLATION = experiment.use_interpolation
+    config.PROGRESSIVE_PROFILING = experiment.progressive_profiling
 
     reporter = None
     if experiment.reporter == "tinymembench":
